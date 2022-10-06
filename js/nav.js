f091e089a8f7518a30c0e065d07a69e2b62e7180
Gwada33 <gwadagwada22@gmail.com>
2022-10-06 16:20:24 -0400

    first commit

---
+++ js/nav.js
@@ -0,0 +1,18 @@
+document.getElementById("nav-container").innerHTML = `<!DOCTYPE html>
+<nav class="navjs">
+<ul>
+    <li><a href="launcher.html" class="ade">Launcher</a></li>
+    <li><a href="tebex" class="ade">Shop</a></li>
+    <li><a href="https://faq.hanadia.fr" class="ade">FAQ</a></li>
+</ul>
+<button>Telecharger</button>
+</nav>`
+
+document.head.innerHTML = `
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title> | Hanadia </title>
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/launcher.css">
+<script src="jquery.js"></script>`
