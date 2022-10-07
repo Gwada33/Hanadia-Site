3619b4310218515fdfc946ba32fe0e987a6ac300
Gwada33 <gwadagwada22@gmail.com>
2022-10-07 15:05:32 -0400

    commit

---
+++ faq.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title> | Hanadia </title>
+      <link rel="stylesheet" href="css/index.css">
+      <link rel="stylesheet" href="css/launcher.css">
+      <script src="jquery.js"></script>`
+</head>      
+<body>      
+</body>
+</html>
---
+++ js/404.js
@@ -0,0 +1,4 @@
+
+if(document.location.pathname = `http://127.0.0.1:5500/`){
+    document.location.replace("http://127.0.0.1:5500/launcher.html")
+}
