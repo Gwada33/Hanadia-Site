adf833d2e191dee7386c84435891a067e7a006af
Gwada33 <104599356+Gwada33@users.noreply.github.com>
2022-10-16 07:30:38 -0400

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,42 @@
-document.getElementById("nav-container").innerHTML = `<!DOCTYPE html>
+document.getElementById("nav-container").innerHTML = `
 <nav class="navjs">
 <ul>
+<div onclick="action();" class="chevron">
+<span class="lol1"></span>
+<span class="lol2"></span>
+<span class="lol3"></span>
+</div>
     <li><a href="launcher.html" class="ade">Launcher</a></li>
     <li><a href="tebex" class="ade">Shop</a></li>
     <li><a href="https://faq.hanadia.fr" class="ade">FAQ</a></li>
 </ul>
 <button>Telecharger</button>
-</nav>`
+</nav>
 
-document.head.innerHTML = `
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title> | Hanadia </title>
-<link rel="stylesheet" href="css/index.css">
-<link rel="stylesheet" href="css/launcher.css">
-<script src="jquery.js"></script>`
+
+<div id="long-nav">
+<ul>
+    <li><a href="launcher.html" class="ade">Launcher</a></li>
+    <li><a href="tebex" class="ade">Shop</a></li>
+    <li><a href="https://faq.hanadia.fr" class="ade">FAQ</a></li>
+</ul>
+</div>`
+
+let truelify = false;
+
+document.getElementById("long-nav").style.transform = "translate('-100%')"
+ 
+
+function action(){
+    if(truelify === false){
+        document.getElementById("long-nav").style.opacity = 1;
+        document.getElementById("long-nav").style.transform = "translateX('100%')"
+        truelify = true;
+        console.log("1")
+    } else if(truelify === true){
+        document.getElementById("long-nav").style.opacity = 0;
+        document.getElementById("long-nav").style.transform = "translateX('-100%')"
+        truelify = false;
+        console.log("2")
+    }
+}
